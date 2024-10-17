0c11eb2543787edd897ec976bef532f1fe22aede
Jiahao Lu <99593334+HughLujh@users.noreply.github.com>
2024-10-17 18:25:23 +1030

    Update UserRepository.java

--- src/main/java/app/blog/repository/UserRepository.java
+++ src/main/java/app/blog/repository/UserRepository.java
@@ -9,4 +9,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
 
     User findByName(String username);
+
+     User findUserByid(Long id);
 }
