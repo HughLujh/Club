fe11dc2563656d736340fae490800b981ae90e4e
Jiahao Lu <Hughlujh@gmail.com>
2024-12-12 16:05:08 +1030

    added unit test on register user successfully

--- src/test/java/app/blog/unitTesting/serviceTests/UserServiceTests.java
+++ src/test/java/app/blog/unitTesting/serviceTests/UserServiceTests.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.mockito.Mockito.*;
 
@@ -23,27 +24,23 @@ public class UserServiceTests {
     private static final String TEST_NAME = "testservice1";
     private static final String TEST_EMAIL = "testservice1@example.com";
     private static final String TEST_PASSWORD = "Password1234!";
-
     private static final String TEST_IMAGEURL = "TestImageUrl";
     private User user;
 
     @BeforeEach
     void setUp() {
-        // Create a UserDto object with test data before running the tests
-        user = user.builder().name(TEST_NAME).email(TEST_EMAIL).password(TEST_PASSWORD).imageUrl(TEST_IMAGEURL).build();
+        // Create a User object with test data before running the tests
+        user = User.builder().name(TEST_NAME).email(TEST_EMAIL).password(TEST_PASSWORD).imageUrl(TEST_IMAGEURL).build();
     }
 
     @Test
     public void registerUserSuccess() {
         // Mock the userRepository.findByEmail method to return null,
         // simulating that no user exists with the provided email
-        when(userRepository.findByEmail(user.getEmail())).thenReturn(null);
+        lenient().when(userRepository.findByEmail(user.getEmail())).thenReturn(null);
 
-        // Call the registerUser method on the userService with the userDto
+        // Call the registerUser method on the userService with the user
         authService.save(user);
-
-        // Verify that the findByEmail method was called exactly once with the given email
-        verify(userRepository, times(1)).findByEmail(user.getEmail());
 
         // Verify that the save method was called exactly once with any User object
         verify(userRepository, times(1)).save(any(User.class));
